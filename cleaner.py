facc7c04260fdc669f598e1d7a026e2167d4620a
Ivan Ryan <ivan@ivanryan.dev>
2023-11-17 15:53:17 +0100

    update

--- cleaner.py
+++ cleaner.py
@@ -10,8 +10,14 @@ class Item:
     def __lt__(self, other):
         return self.name < other.name
 
+    def __eq__(self, other):
+        return self.name == other.name and self.classname == other.classname
+
+    def __hash__(self):
+        return hash((self.name, self.classname))
+
 def read_csv(file_path):
-    groups = defaultdict(list)
+    groups = defaultdict(set)
     try:
         with open(file_path, mode='r', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile)
@@ -22,18 +28,22 @@ def read_csv(file_path):
                     if name and i + 1 < len(row):
                         classname = row[i + 1].strip()
                         if classname:
-                            group_key = headers[i]
-                            groups[group_key].append(Item(name, classname))
+                            group_key = headers[i].strip()
+                            item = Item(name, classname)
+                            groups[group_key].add(item)
     except FileNotFoundError:
         print(f"Error: The file '{file_path}' was not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
     return groups
 
-def sort_groups(groups):
+def sort_and_deduplicate_groups(groups):
     sorted_groups = {}
+    seen = set()
     for key, items in groups.items():
-        sorted_groups[key.strip()] = sorted(items, key=lambda item: item.name)
+        unique_items = [item for item in items if item not in seen]
+        seen.update(unique_items)
+        sorted_groups[key] = sorted(unique_items, key=lambda item: item.name)
     return sorted_groups
 
 def convert_to_json(groups):
@@ -56,11 +66,16 @@ def write_csv(file_path, groups):
             # Write the headers
             headers = [key for key in groups.keys() for _ in (0, 1)]
             writer.writerow(headers)
-            # Write the sorted data
-            rows = zip(*[[(item.name, item.classname) for item in items] for items in groups.values()])
-            for row in rows:
-                flattened_row = [element for pair in row for element in pair]
-                writer.writerow(flattened_row)
+            # Write the sorted and deduplicated data
+            max_length = max(len(items) for items in groups.values())
+            for i in range(max_length):
+                row = []
+                for items in groups.values():
+                    if i < len(items):
+                        row.extend([items[i].name, items[i].classname])
+                    else:
+                        row.extend(['', ''])
+                writer.writerow(row)
     except Exception as e:
         print(f"An error occurred while writing to CSV: {e}")
 
@@ -70,13 +85,13 @@ def main():
     output_csv_file = 'sorted_data.csv'
 
     groups = read_csv(input_file)
-    sorted_groups = sort_groups(groups)
+    sorted_groups = sort_and_deduplicate_groups(groups)
     json_output = convert_to_json(sorted_groups)
     write_json(output_json_file, json_output)
     write_csv(output_csv_file, sorted_groups)
 
     print(f"Structured JSON data written to {output_json_file}")
-    print(f"Sorted CSV data written to {output_csv_file}")
+    print(f"Sorted and deduplicated CSV data written to {output_csv_file}")
 
 if __name__ == "__main__":
     main()
